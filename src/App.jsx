b6228a38cbbfdc76bcd456b10dc10f10a14d99a7
Anushkasrivastava07 <anushkasrivastava020@gmail.com>
2026-06-12 01:10:57 +0530

    "fix responsive design"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-jsx
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -13,38 +12,30 @@ function App() {
         justifyContent: "space-between", 
         padding: "16px 40px", 
         background: "#111827", 
-        color: "white", 
-        alignItems: "center", 
+        color: "white",
+        alignItems: "center",
         boxShadow: "0 2px 4px rgba(0,0,0,0.1)"
       }}>
-        <h2 style={{
-          margin: 0, 
-          fontSize: "24px", 
-          fontWeight: "700", 
-          color: "#ffffff"
-        }}>
+        <h2 style={{margin: 0, fontSize: "24px", fontWeight: "700"}}>
           DevJobs
         </h2>
         <div style={{display: "flex", gap: "30px"}}>
-          <Link to="/" style={{
-            color: "white", 
-            textDecoration: "none", 
-            fontSize: "16px"
-          }}>
+          <Link 
+            to="/" 
+            style={{color: "white", textDecoration: "none", fontSize: "16px"}}
+          >
             Home
           </Link>
-          <Link to="/post-job" style={{
-            color: "white", 
-            textDecoration: "none", 
-            fontSize: "16px"
-          }}>
+          <Link 
+            to="/post-job" 
+            style={{color: "white", textDecoration: "none", fontSize: "16px"}}
+          >
             Post a Job
           </Link>
-          <Link to="/login" style={{
-            color: "white", 
-            textDecoration: "none", 
-            fontSize: "16px"
-          }}>
+          <Link 
+            to="/login" 
+            style={{color: "white", textDecoration: "none", fontSize: "16px"}}
+          >
             Login
           </Link>
         </div>
